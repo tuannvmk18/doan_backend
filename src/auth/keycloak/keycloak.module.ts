356c6c09031c22a0184e70258d6190fc1a25930c
Nguyen Van Minh Tuan <tuan01102000@gmail.com>
2021-12-27 21:23:29 +0700

    xoa file env va module thua

--- src/auth/keycloak/keycloak.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { KeycloakProvider } from './keycloak.provider';
-
-@Module({
-  providers: [KeycloakProvider],
-  exports: [KeycloakProvider],
-})
-export class KeycloakModule {}
